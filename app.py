82527250c20db02e6b3086ae9ab778b2ad5347ab
Chase Westover <chasejwestover@gmail.com>
2021-03-04 14:43:12 -0600

    squashed bug

--- app.py
+++ app.py
@@ -9,26 +9,27 @@ app.config['SECRET_KEY'] = "secret"
 debug = DebugToolbarExtension(app)
 
 stories = [silly_story, excited_story]
-story = None
+story = []
+
 
 @app.route('/')
 def index():
     t1 = silly_story.name
     t2 = excited_story.name
-    return render_template('home.html', t1=t1 , t2=t2)
+    return render_template('home.html', t1=t1, t2=t2)
+
 
 @app.route('/questions')
 def form():
     story_name = request.args['story']
     for s in stories:
         if s.name.startswith(story_name):
-            story = s
-            print("made it")
-    lst_words = story.prompts
-    return render_template("questions.html", lst_words = lst_words)
+            story.append(s)
+    lst_words = story[0].prompts
+    return render_template("questions.html", lst_words=lst_words)
+
 
 @app.route('/story')
 def get_story():
-    print(story)
-    text = story.generate(dict(request.args))
-    return render_template('story.html', text = text)
+    text = story[0].generate(dict(request.args))
+    return render_template('story.html', text=text)
